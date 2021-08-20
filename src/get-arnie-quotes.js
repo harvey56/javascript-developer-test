bdfc86f78498b1ff72161e69b37270d07675cb16
Harvey56 <hervel56@gmail.com>
2021-08-20 13:09:32 +1000

    smokeball JS test results

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -3,7 +3,32 @@ const { httpGet } = require('./mock-http-interface');
 const getArnieQuotes = async (urls) => {
   // TODO: Implement this function.
   // return results;
+
+  const requestResults = urls.map(async (url) => await(httpGet(url)));
+
+  const results = await Promise.all(requestResults);
+
+  const processResults = results.map(result => {
+    if(result.status === 200){
+      return processSuccess(JSON.parse(result.body).message);
+    }
+    else{ // 4xx - 500
+      return processFailure(JSON.parse(result.body).message);
+    }
+  });
+
+  return processResults;
 };
+
+
+
+const processSuccess = (quote) => {
+  return ({"Arnie Quote": quote});
+}
+
+const processFailure = (quote) => {
+  return ({"FAILURE": quote});
+}
 
 module.exports = {
   getArnieQuotes,
